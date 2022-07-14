f5875759fd829b6e5f452a0d7109abb923ff6462
Vishal <vishal@openspaceservices.com>
2022-07-14 18:34:38 +0530

    Clearing Unwanted Files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Report1 from './components/Report1';
 import Report2 from './components/Report2';
 import Error from './components/Error';
@@ -7,6 +7,10 @@ import Error from './components/Error';
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to='/'>Report 1</Link>
+        <Link to='/1'>Report 2</Link>
+      </nav>
       <Routes>
         <Route path='/' element={<Report1 />}></Route>
         <Route path='/1' element={<Report2 />}></Route>
